72445fe881de7251715f5e47029a5821034daf2c
nvj1d <najid99m@gmail.com>
2022-07-02 16:37:09 +0100

    struct_philo

--- philo.h
+++ philo.h
@@ -6,21 +6,31 @@
 /*   By: mnajid <mnajid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 14:58:16 by mnajid            #+#    #+#             */
-/*   Updated: 2022/07/02 15:17:36 by mnajid           ###   ########.fr       */
+/*   Updated: 2022/07/02 16:36:50 by mnajid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
 
 typedef struct s_philosopher
 {
-	int	rang;
+	pthread_t		thread_id;
+	int				nb_forks;
+	int				rang;
+	int				right_fork;
+	int				left_fork;
+	int				nb_eat;
+	struct s_data	*data;
 }	t_philosopher;
 
+typedef struct s_data{
+	int	nb_philosophers;
+}	t_data;
 #endif
 
 // eat xor sleep xor think
